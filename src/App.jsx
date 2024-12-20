ec84f5d3a3011ec567b2478ed9a0d2ad23532758
Bello qudus <qudusbello51@gmail.com>
2024-12-20 01:24:12 +0100

    another one

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Home from "./Pages/Home";
-import NotFound from "./Pages/NotFound";
-import Recipe from "./Pages/Recipe";
+
+const Header = React.lazy(() => import("./Components/Header"));
+const Home = React.lazy(() => import("./Pages/Home"));
+const NotFound = React.lazy(() => import("./Pages/NotFound"));
+const Recipe = React.lazy(() => import("./Pages/Recipe"));
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Recipe />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading Header...</div>}>
+        <Header />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Recipe />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
